fc99d8d9182f4c9725c2f42a8bc6318ba3eedfd6
Junior <vilmarmjrr@gmail.com>
2021-04-10 14:31:36 -0300

    feat: add routing structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumsComponent } from './pages/albums/albums.component';
 import { LoginComponent } from './pages/login/login.component';
+import { MainComponent } from './shared/layouts/main/main.component';
 
 const routes: Routes = [
   {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'login'
-  },
-  {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      { path: '', redirectTo: 'albums', pathMatch: 'full' },
+      { path: 'albums', component: AlbumsComponent }
+    ]
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { LoginComponent } from './pages/login/login.component';
 import { MatInputModule } from '@angular/material/input';
+import { MainComponent } from './shared/layouts/main/main.component';
+import { AlbumsComponent } from './pages/albums/albums.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, MainComponent, AlbumsComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule, MatCardModule, MatInputModule],
   providers: [],
   bootstrap: [AppComponent]
