eddeeaf241e68ee1ad30025b2733ea863a6897c8
Junior <vilmarmjrr@gmail.com>
2021-04-11 19:52:08 -0300

    feat: add take pipes

--- src/app/services/impl/json-placeholder-albums.service.ts
+++ src/app/services/impl/json-placeholder-albums.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, take } from 'rxjs/operators';
 import { baseUrl } from 'src/app/config/api-config';
 import { AlbumNotFoundError } from 'src/app/errors/album-not-found-error';
 import { AlbumModel } from 'src/app/models/album.model';
@@ -12,10 +12,13 @@ export class JsonPlaceholderAlbumsService implements AlbumsService {
   constructor(private httpClient: HttpClient) {}
 
   fetchAlbums(userId: number): Observable<AlbumModel[]> {
-    return this.httpClient.get<AlbumModel[]>(`${baseUrl}/albums?userId=${userId}`);
+    return this.httpClient.get<AlbumModel[]>(`${baseUrl}/albums?userId=${userId}`).pipe(take(1));
   }
 
   fetchAlbumById(id: number): Observable<AlbumModel | AlbumNotFoundError> {
-    return this.httpClient.get<AlbumModel>(`${baseUrl}/albums/${id}`).pipe(catchError(() => of(new AlbumNotFoundError())));
+    return this.httpClient.get<AlbumModel>(`${baseUrl}/albums/${id}`).pipe(
+      catchError(() => of(new AlbumNotFoundError())),
+      take(1)
+    );
   }
 }
--- src/app/services/impl/json-placeholder-photos.service.ts
+++ src/app/services/impl/json-placeholder-photos.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 import { baseUrl } from 'src/app/config/api-config';
 import { Photo } from 'src/app/models/photo.model';
 import { PhotosService } from '../photos.service';
@@ -11,10 +11,13 @@ export class JsonPlaceholderPhotosService implements PhotosService {
   constructor(private readonly httpClient: HttpClient) {}
 
   fetchPhotos(albumId: number, { page, limit }): Observable<Photo[]> {
-    return this.httpClient.get<Photo[]>(`${baseUrl}/albums/${albumId}/photos?_page=${page}&_limit=${limit}`);
+    return this.httpClient.get<Photo[]>(`${baseUrl}/albums/${albumId}/photos?_page=${page}&_limit=${limit}`).pipe(take(1));
   }
 
   fetchFirstPhotoFromAlbum(albumId: number): Observable<Photo> {
-    return this.httpClient.get<Photo[]>(`${baseUrl}/photos?albumId=${albumId}&_limit=1`).pipe(map(photos => photos[0]));
+    return this.httpClient.get<Photo[]>(`${baseUrl}/photos?albumId=${albumId}&_limit=1`).pipe(
+      map(photos => photos[0]),
+      take(1)
+    );
   }
 }
