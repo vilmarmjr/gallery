74cdbddc1d2f3e568c48a3c0c3e01b149cf323a0
Junior <vilmarmjrr@gmail.com>
2021-04-11 14:12:50 -0300

    refactor: move baseUrl to a config file

--- src/app/shared/services/albums.service.ts
+++ src/app/shared/services/albums.service.ts
@@ -2,20 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { baseUrl } from '../config/api-config';
 import { AlbumNotFoundError } from '../errors/album-not-found-error';
 import { AlbumModel } from '../models/album.model';
 
 @Injectable()
 export class AlbumsService {
-  private readonly baseUrl = 'https://jsonplaceholder.typicode.com';
-
   constructor(private httpClient: HttpClient) {}
 
   fetchAlbums(userId: number): Observable<AlbumModel[]> {
-    return this.httpClient.get<AlbumModel[]>(`${this.baseUrl}/albums?userId=${userId}`);
+    return this.httpClient.get<AlbumModel[]>(`${baseUrl}/albums?userId=${userId}`);
   }
 
   fetchAlbumById(id: number): Observable<AlbumModel | AlbumNotFoundError> {
-    return this.httpClient.get<AlbumModel>(`${this.baseUrl}/albums/${id}`).pipe(catchError(() => of(new AlbumNotFoundError())));
+    return this.httpClient.get<AlbumModel>(`${baseUrl}/albums/${id}`).pipe(catchError(() => of(new AlbumNotFoundError())));
   }
 }
--- src/app/shared/services/photos.service.ts
+++ src/app/shared/services/photos.service.ts
@@ -2,19 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { baseUrl } from '../config/api-config';
 import { Photo } from '../models/photo.model';
 
 @Injectable()
 export class PhotosService {
-  private readonly baseUrl = 'https://jsonplaceholder.typicode.com';
-
   constructor(private readonly httpClient: HttpClient) {}
 
   fetchPhotos(albumId: number, { page, limit }): Observable<Photo[]> {
-    return this.httpClient.get<Photo[]>(`${this.baseUrl}/albums/${albumId}/photos?_page=${page}&_limit=${limit}`);
+    return this.httpClient.get<Photo[]>(`${baseUrl}/albums/${albumId}/photos?_page=${page}&_limit=${limit}`);
   }
 
   fetchFirstPhotoFromAlbum(albumId: number): Observable<Photo> {
-    return this.httpClient.get<Photo[]>(`${this.baseUrl}/photos?albumId=${albumId}&_limit=1`).pipe(map(photos => photos[0]));
+    return this.httpClient.get<Photo[]>(`${baseUrl}/photos?albumId=${albumId}&_limit=1`).pipe(map(photos => photos[0]));
   }
 }
